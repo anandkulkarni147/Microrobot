a1129d380a4b81e14138ace3585f0f5b9429b3b5
Anand Kulkarni <kulkarnianand147@gmail.com>
2022-12-13 23:14:36 +0000

    added animations on background.js

--- src/Components/Background/background.js
+++ src/Components/Background/background.js
@@ -1,40 +1,70 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import bacteria from "../../Images/bacteria.svg";
 import "./background.css";
 
-const svgVariants = {};
-
 function Background(props) {
   const [posX, setPosX] = useState(450);
+  const [visible, setVisible] = useState(true);
 
-  const handleMovement = () => {
-    for (var i = 100; i < 500; ++i) {
-      setPosX(i);
+  const controls = useAnimation();
+
+  const check = () => {
+    if (!visible) controls.stop();
+    // await controls.start({
+    //   // x: posX,
+    //   // y: 0,
+    //   // transition: {
+    //   //   ease: "circInOut",
+    //   //   duration: 1,
+    //   // },
+    // });
+    if (posX - 150 <= props.pos + 80) {
+      setVisible(false);
+      controls.stop();
+      document.getElementById("bacteria").style.display = "none";
+      console.log("Entered: " + posX);
+    } else {
+      setPosX(posX + 3);
     }
   };
 
+  const animationProps = {
+    initial: { x: 450, y: 0 },
+    show: {
+      x: posX,
+      y: 0,
+      transition: {
+        ease: "linear",
+      },
+    },
+    exit: {
+      x: 0,
+      opacity: 0,
+      scale: 2,
+    },
+  };
+
   return (
-    <div className="bacteria">
-      <motion.img
-        initial={{
-          x: 450,
-          y: -10,
-        }}
-        src={bacteria}
-        animate={{
-          x: posX,
-          y: 0,
-        }}
-        transition={{
-          ease: "linear",
-          // duration: 1,
-          // repeat: Infinity,
-        }}
-        onAnimationComplete={() => {
-          setPosX(posX + 3);
-        }}
-      />
+    <div className="bacteria" id="bacteria">
+      <AnimatePresence>
+        {visible && (
+          <motion.img
+            variants={animationProps}
+            key="modal"
+            initial="initial"
+            src={bacteria}
+            animate="show"
+            exit="exit"
+            // onAnimationStart={() => check()}
+
+            onAnimationComplete={() => check()}
+            onAnimationEnd={() => {
+              document.getElementById("bacteria").style.display = "flex";
+            }}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
